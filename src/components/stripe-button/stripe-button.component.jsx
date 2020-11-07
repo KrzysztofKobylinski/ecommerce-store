28e3c24c01e85f75498704577189753320aaf861
Krzysiek Kobyliński <kkobylinski@bevy.pl>
2020-11-07 14:32:12 +0100

    stripe test integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HkrL0GHBGuAhgxIjJRoFsQMV5udNyOkYMwIGRhBU44M2a2PbQkBko6abL5OVO79oYA0cXXS0Igw7ancdoo3RMFa00WaFSZlxh';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
